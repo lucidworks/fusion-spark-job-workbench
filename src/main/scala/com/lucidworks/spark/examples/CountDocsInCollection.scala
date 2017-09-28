712bcd4ad2b033d4a9fcf7f5fd03ab2f7e669cc0
kiranchitturi <kiran.chitturi@lucidworks.com>
2017-09-28 11:46:48 -0700

    First push with example and working gradle

---
+++ src/main/scala/com/lucidworks/spark/examples/CountDocsInCollection.scala
@@ -0,0 +1,26 @@
+package com.lucidworks.spark.examples
+
+import org.apache.spark.sql.SparkSession
+import org.slf4j.LoggerFactory
+
+class CountDocsInCollection
+
+object CountDocsInCollection {
+
+  val logger = LoggerFactory.getLogger(classOf[CountDocsInCollection])
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession
+        .builder
+        .appName("TestExample")
+        .getOrCreate()
+
+    logger.info(s"Spark conf dump: ${spark.sparkContext.getConf.toDebugString}")
+    logger.info(s"System properties: ${System.getProperties.keySet()}")
+    logger.info(s"Env properties: ${System.getenv().keySet()}")
+    val df = spark.read.format("solr").option("collection", "logs").load()
+    df.count()
+    logger.info("Document count is  " + df.count())
+  }
+
+}
