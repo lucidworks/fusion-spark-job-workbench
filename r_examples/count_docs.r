297418a9fe0d02a94548d8b52ea34ed717df3459
kiranchitturi <kiran.chitturi@lucidworks.com>
2017-10-02 09:38:01 -0700

    Remove R examples, SparkR not supporting external jars yet

--- r_examples/count_docs.r
+++
@@ -1,7 +0,0 @@
-library(SparkR)
-
-sparkR.session(appName = "R Spark SQL basic example")
-
-df1 <- read.df("solr", collection = "logs")
-
-print("Number of documents in collection logs is " + count(df1))
